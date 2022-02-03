11db1c043d7e9128f24a5bd401270c4525f46aa9
kukkusuresh <93712206+kukkusuresh@users.noreply.github.com>
2022-02-03 17:36:08 +0530

    Add files via upload

--- logform.js
+++ logform.js
@@ -1,34 +1,47 @@
-let psw=document.getElementById("psw");
-function validate(){
-    if(user.value==""){
-    
-        alert("cannot be empty");
-        return false;
-        }
-        else if(pwd.value==""){
-            alert("password cannot be blank");
-            return false;
-        
-        }
-        else if(pwd.value.length<=8){
-            alert("password is too short");
-            return false;
-        }
-        else{
-            return true;
-        }}
-        
-        
-        
-           
-                   
-              
-// Get the modal
-var modal = document.getElementById('id01');
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "error";
-    }
+function validate(){
+ // document.getElementById('id01').textContent="";
+  document.getElementById("eerror").textContent="";
+  document.getElementById("perror").textContent="";
+  var x=verifyemail();
+  var z=verifypass();
+  return z && x;
+}
+function verifyemail() {
+  var e = document.getElementById("email");
+  var validRegex =/^([a-zA-Z0-9\.-]+)@([a-zA-Z0-9-]+)\.([a-z]{2,8})(.[a-z]{2,8})?$/
+  if (validRegex.test(e.value)){
+      return true;
+  }  
+  else {
+      document.getElementById("eerror").textContent="Invalid E-mail ID";
+      document.getElementById("eerror").style.color="red";
+      e.style.border="2px solid red";
+      e.value="";
+      return false;
+  }
+}
+
+
+function verifypass() {
+  var p = document.getElementById("psw");
+  var passw =  /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/;
+  if (psw.value.match(passw)) {
+      return true;
+  }
+  else {
+      document.getElementById("perror").textContent="Input Password with 8 to 15 characters which contain at least one lowercase letter, one uppercase letter, one numeric digit, and one special character";
+      document.getElementById("perror").style.color="red";
+      p.style.border="2px solid red";
+      psw.value="";
+      return false;
+  }
+}
+function colorchange() {
+  var f = document.getElementById("frmLogin");
+  f.style.backgroundColor = "pink";
+}
+function colorback() {
+  var f = document.getElementById("frmLogin");
+  f.style.backgroundColor = "transparent";
 }
